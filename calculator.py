bde83a71d5173225be8a92dc720ca7e671a54448
Wanjaa <wanjaahope@gmail.com>
2025-10-05 13:12:25 +0300

    Group guide

--- calculator.py
+++ calculator.py
@@ -1,1 +1,24 @@
 print("Welcome to our group calculator project!")
+
+#==== Addition function here =====
+
+
+
+
+#==== Subtraction function here =====
+
+
+
+
+#==== Multiplication function here =====
+
+
+
+
+#==== Division function here =====
+
+
+
+
+
+#==== Validation funtion here ====
