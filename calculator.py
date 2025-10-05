76acfe6a16d8245bc6cece210a678b986152a186
muasya669 <muasyamartha8@gmail.com>
2025-10-05 17:05:50 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -11,7 +11,11 @@ print("Welcome to our group calculator project!")
 
 
 #==== Multiplication function here =====
-
+#multiplication of two numbers
+num1= float(input("Enter the first number:"))
+num2= float(input ("Enter the second number:"))
+multiplication_result= num1 * num2
+print("The result of multiplication is: ",multiplication_result
 
 
 
