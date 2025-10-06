5c49dc4018cc31821c71adf69f886fb308d62410
adele444 <adelemiyoro@gmail.com>
2025-10-06 23:01:36 +0300

    Subtraction function

--- calculator.py
+++ calculator.py
@@ -7,7 +7,12 @@ print("Welcome to our group calculator project!")
 
 #==== Subtraction function here =====
 
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
 
+subtraction_result = num1 - num2
+
+print(num1, "-", num2, "=", subtraction_result)
 
 
 #==== Multiplication function here =====
